03493b26041045c89348a5d1e9e11b1482105969
goodnessugo <emusical123@gmail.com>
2023-08-27 13:02:22 +0100

    update

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className=' w-full h-44 bg-gray-700 gap-4 grid grid-cols-1 sm:grid-cols-2 text-center py-20 text-white font-bold'>
+            <div className=' w-full h-44 bg-gray-700 gap-4 grid grid-cols-1 sm:grid-cols-2 text-center py-20 text-white text-sm font-bold'>
 
                 <h1>Copyright © 2023 <span className='text-yellow-500 underline'>Goodness.</span> All Rights Reserved.</h1>
                 <h1>Designed by <span className='text-yellow-500 underline'>Goodness Ugochukwu George</span></h1>
